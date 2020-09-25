5197e6dfebea04c293d2742084d98fb182ece27e
Chris Carbonell <ccarbonell228@gmail.com>
2020-09-24 23:03:40 -0500

    clean data

--- build/python/wbn_email_name.py
+++ build/python/wbn_email_name.py
@@ -24,142 +24,291 @@ gbl_str_path_output = "/python/output/" # end with last /
 
 # randomize
 gbl_str_gender = random.choice(['boys','girls'])
-gbl_int_rank = random.randint(1,5000)
+gbl_int_max = 5000
+gbl_int_rank = random.randint(1,gbl_int_max)
 
-# wolfram alpha client
-wc = wolframalpha.Client(secrets.gbl_str_app_id)
-question = " ".join(["#" + str(gbl_int_rank), gbl_str_gender, "name"]) # ie "#1 boys name"
-print(question)
-res = wc.query(question)
+# funcs
 
-# save raw data
+def get_dict_data(str_gender, int_rank):
+    '''
+    organize the wolfram data into dict
 
-dict_raw = {}
-int_pod = 0
+    returns:
+    dict_data (dict): key = my key, value = raw vale from wolfram alpha API
+    '''
 
-for pod in res.pods:
+    # wolfram alpha client
+    wc = wolframalpha.Client(secrets.gbl_str_app_id)
     
-    dict_raw[int_pod] = {}
-    
-    sub = next(pod.subpods) # there's only one
-    if sub['plaintext'] is not None:
-        dict_raw[int_pod] = sub['plaintext'].split('\n')
-    else:
-        dict_raw[int_pod] = {'@src': sub['img']['@src'], '@alt': sub['img']['@alt']}
-    
-    int_pod = int_pod + 1
+    # question 1: #n [boys/girls] name
+    question = " ".join(["#" + str(int_rank), str_gender, "name"])
+    print(question)
+    res = wc.query(question)
 
-# downlaod images
+    # set up landing spots
+    dict_data = {}
+    int_pod = 0
 
-str_path_img_history = os.path.join(gbl_str_path_output, gbl_str_gender + " " + str(gbl_int_rank).zfill(5) + " " + dict_raw[1][0] + "-history.jpg")
-str_path_img_age = os.path.join(gbl_str_path_output, gbl_str_gender + " " + str(gbl_int_rank).zfill(5) + " " + dict_raw[1][0] + "-age.jpg")
+    # find my target info
+    # some results are missing some info
 
-urllib.request.urlretrieve(dict_raw[3]['@src'], str_path_img_history)
-urllib.request.urlretrieve(dict_raw[5]['@src'], str_path_img_age)
+    # loop over pods
+    for pod in list(res.pods):
+        
+        # if hiearchy to find targets
 
-# send email
-# https://realpython.com/python-send-email/
+        # name and number
+        if pod['@id'] == 'Result' and pod['@scanner'] == 'Ordinal' and pod['subpod']['@title'] == '':
+            
+            ls_plaintext = pod['subpod']['plaintext'].split('\n')
+            
+            dict_data['name'] = ls_plaintext[0] # "Chris"
+            
+            if len(ls_plaintext) > 1:
+                dict_data['number'] = ls_plaintext[1][1:-1] # "437 people per year"
 
-sender_email = secrets.gbl_str_un
+        # stats
 
-# get receiver email from CSV
-# receiver_email = ["john_doe@gmail.com"]
-with open('/python/input/emails.csv', newline='') as f:
-    reader = csv.reader(f)
-    receiver_email = [row[0] for row in reader]
+        elif pod['@id'] == 'BasicInformation:GivenNameData':
 
-message = MIMEMultipart("alternative")
-message["Subject"] = "Baby Name: " + dict_raw[1][0]
-message["From"] = sender_email
-message["To"] = ", ".join(receiver_email)
+            ls_plaintext = pod['subpod']['plaintext'].split('\n')
 
-if 8 in dict_raw.keys():
-    str_meaning = dict_raw[8][0]
-else:
-    str_meaning = "[no meaning found]"
+            for str_plaintext in ls_plaintext:
+                if str_plaintext.startswith('rank | '):
+                    # eg "rank | 613th"
+                    dict_data['rank'] = str_plaintext.split("|")[1].strip() # "613th"
+                elif str_plaintext.startswith('fraction | '):
+                    # eg "fraction | 1 in 4120 people (0.024%)""
+                    dict_data['fraction'] = str_plaintext.split("|")[1].strip() # "1 in 4120 people (0.024%)"
+                elif str_plaintext.startswith('number | '):
+                    # eg "number | 437 people per year"
+                    dict_data['number'] = str_plaintext.split("|")[1].strip() # "437 people per year"
+                elif str_plaintext.startswith('('):
+                    # eg "(US data based on 2018 births and other SSA registrations in the US)"
+                    dict_data['source_rank'] = str_plaintext[1:-1] # "US data based on 2018 births and other SSA registrations in the US"
+                else:
+                    pass
 
-text = """
-""" + dict_raw[1][0] + """
-""" + str_meaning
+        elif pod['@id'] == 'CurrentEstimates:GivenNameData':
 
-# improvement: need error handling (eg meaning)
-html = """\
-<html>
-  <head></head>
-  <body>
-    <h1>""" + dict_raw[1][0] + """</h1>
-    <b style='color:blue'><i>""" + str_meaning + """</i></b>
+            ls_plaintext = pod['subpod']['plaintext'].split('\n')
 
-    <h2>Random Stats</h2>
-    <ul>
-        <li>""" + dict_raw[2][0].split("|")[1].strip() + """ most popular</li>
-        <li>""" + dict_raw[2][1].split("|")[1].strip() + """</li>
-        <li>""" + dict_raw[2][2].split("|")[1].strip() + """</li>
-        <li>""" + dict_raw[4][0].replace("|","=") + """</li>
-        <li>""" + dict_raw[4][3].replace("|","=") + """</li>
-    </ul>
-    
-    <h2>""" + dict_raw[3]['@alt'] + """</h2>
-    <img src="cid:image1">
-    
-    <h2>""" + dict_raw[5]['@alt'] + """</h2>
-    <img src="cid:image2">"""
-    
-if 7 in dict_raw.keys():
+            for str_plaintext in ls_plaintext:
+        
+                if str_plaintext.startswith('expected total number alive today | '):
+                    # eg "expected total number alive today | 126812 people"
+                    dict_data['alive'] = str_plaintext.replace("|","=") # "expected total number alive today = 126812 people"
+                elif str_plaintext.startswith('most common age | '):
+                    # eg "most common age | 49 years"
+                    dict_data['mode_age'] = str_plaintext.replace("|","=") # "most common age = 49 years"
+                elif str_plaintext.startswith('('):
+                    # eg "(using standard US mortality data)"
+                    dict_data['mortality_rank'] = str_plaintext[1:-1] # "using standard US mortality data"
+
+        # history
+        elif pod['@id'] == 'History:GivenNameData':
+            if pod['subpod']['img']['@alt'] == 'History for US births Fraction':
+                dict_data['history'] = pod['subpod']['img'] # dict = {'@src': [url],'@alt': 'History for US births Fraction'}
+
+        # age
+        elif pod['@id'] == 'AgeDistribution:GivenNameData':
+            if pod['subpod']['img']['@alt'] == 'Estimated current age distribution':
+                dict_data['age'] = pod['subpod']['img'] # dict = {'@src': [url],'@alt': 'Estimated current age distribution'}
+
+        # alternates
+        elif pod['@id'] == 'AlternateVersions:GivenNameData':
+            dict_data['alternate'] = pod['subpod']['plaintext'].split(' | ')
+
+        # famous
+        elif pod['@id'] == 'NotablePeopleWithName:GivenNameData':
+            dict_data['famous'] = pod['subpod']['plaintext'].split('\n')
+
+        # origin
+        elif pod['@id'] == 'NameMeaning:GivenNameData':
+            dict_data['origin'] = pod['subpod']['plaintext']
+
+        # similar ranks (ordinal)
+        elif pod['@id'] == 'OrdinalRankings':
+            # eg '⋮ | | | \n611 | Abdiel | 439 people/yr | \n612 | Hamza | 438 people/yr | \n613 | Chris | 437 people/yr | \n614 | Alfredo | 437 people/yr | \n615 | Finnley | 436 people/yr | \n⋮'
+            
+            ls_ordinal = pod['subpod']['plaintext'].split('\n')[1:-1] # first and last are nonsense
+
+            if ls_ordinal[0][0] == "⋮":
+                ls_ordinal.remove(ls_ordinal[0])
+            elif ls_ordinal[-1] == "⋮":
+                ls_ordinal.remove(ls_ordinal[-1])
+            else:
+                pass
+
+            dict_data['ordinal'] = ls_ordinal
+
+        else:
+            pass
+
+        # if text not found, fill with error text
+        for str_key in ['name', 'origin', 'rank', 'fraction', 'number', 'alive', 'mode_age', 'source_rank', 'mortality_rank']:
+            if str_key not in dict_data.keys():
+                dict_data[str_key] = "no data found for: " + str_key
+
+        # if object not found, fill with None
+        for str_key in ['history', 'age', 'famous', 'alternate', 'ordinal']:
+            if str_key not in dict_data.keys():
+                dict_data[str_key] = None
+
+    return dict_data
+
+if __name__ == "__main__":
+
+    # get data
+    dict_data = get_dict_data(gbl_str_gender, gbl_int_rank)
+
+    # downlaod images
+
+    if dict_data['history'] is not None:
+        str_path_img_history = os.path.join(gbl_str_path_output, gbl_str_gender + " " + str(gbl_int_rank).zfill(5) + " " + dict_data['name'] + "-history.jpg")
+        urllib.request.urlretrieve(dict_data['history']['@src'], str_path_img_history)
+
+    if dict_data['age'] is not None:
+        str_path_img_age = os.path.join(gbl_str_path_output, gbl_str_gender + " " + str(gbl_int_rank).zfill(5) + " " + dict_data['name'] + "-age.jpg")
+        urllib.request.urlretrieve(dict_data['age']['@src'], str_path_img_age)
+
+    # send email
+    # https://realpython.com/python-send-email/
+
+    sender_email = secrets.gbl_str_un
+
+    # get receiver email from CSV
+    # receiver_email = ["john_doe@gmail.com"]
+    with open('/python/input/emails.csv', newline='') as f:
+        reader = csv.reader(f)
+        receiver_email = [row[0] for row in reader]
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "Baby Name: " + dict_data['name']
+    message["From"] = sender_email
+    message["To"] = ", ".join(receiver_email)
+
+    text = """\
+    """ + dict_data['name'] + """\
+    """ + dict_data['origin']
+
+    # name stats
+    html = """\
+    <html>
+    <head></head>
+    <body>
+        <h1>""" + dict_data['name'] + """ (""" + gbl_str_gender[:-1] + """)</h1>"""
+
+    if not dict_data['origin'].startswith("no data found for: "):
+        html = html + """<b style='color:blue'><i>""" + dict_data['origin'] + """</i></b>
+        """
+
     html = html + """\
-        <h2>Famous Examples</h2>
+        <h2>Random Stats</h2>
         <ul>
-        """ + "\n".join(["<li>" + str_example + "</li>" for str_example in dict_raw[7]]) + """
+            <li>""" + dict_data['rank'] + """ most popular</li>
+            <li>""" + dict_data['fraction'] + """</li>
+            <li>""" + dict_data['number'] + """</li>
+            <li>""" + dict_data['alive'] + """</li>
+            <li>""" + dict_data['mode_age'] + """</li>
         </ul>
         """
+
+    # birth history graph
+    if dict_data['history'] is not None:
+        html = html + """\
+            <h2>History for US Births (Fraction)</h2>
+            <img src="cid:image1">
+            """
     
+    # age distribution graph
+    if dict_data['age'] is not None:
+        html = html + """\
+            <h2>Estimated Current Age Distribution</h2>
+            <img src="cid:image2">
+            """
+    
+    # famous examples
+    if dict_data['famous'] is not None:
+        html = html + """\
+            <h2>Famous Examples</h2>
+            <ul>
+            """ + "\n".join(["<li>" + str_example + "</li>" for str_example in dict_data['famous']]) + """
+            </ul>
+            """
+
+    # alternates
+    if dict_data['alternate'] is not None:
+        html = html + """\
+            <h2>Alternates</h2>
+            <ul>
+            """ + "\n".join(["<li>" + str_alternate + "</li>" for str_alternate in dict_data['alternate']]) + """
+            </ul>
+            """
+
+    # ordinal
+    if dict_data['ordinal'] is not None:
+        html = html + """\
+            <h2>Similar Rankings</h2>
+            <ul>
+            """ + "\n".join(["<li>" + str_ordinal + "</li>" for str_ordinal in dict_data['ordinal']]) + """
+            </ul>
+            """
+
+    # finish with Sources    
     html = html + """\
-    <h2>Sources</h2>
-    <ul>
-        <li>""" + dict_raw[2][3][1:-1] + """</li>
-        <li>""" + dict_raw[4][4][1:-1] + """</li>
-    </ul>
-  </body>
-</html>
-"""
+        <h2>Sources</h2>
+        <ul>
+            <li>Randomly choosing from the top """ + str(gbl_int_max) + """ names on WoflramAlpha:
+            <a href="https://www.wolframalpha.com/input/?i=%23""" + str(gbl_int_rank) + """+""" + gbl_str_gender + """+name"">#""" + str(gbl_int_rank) + " " + gbl_str_gender + """ name</a></li>
+            <li>""" + dict_data['source_rank'] + """</li>
+            <li>""" + dict_data['mortality_rank'] + """</li>
+        </ul>
+    </body>
+    </html>
+    """
 
-# Record the MIME types of both parts - text/plain and text/html.
-part1 = MIMEText(text, 'plain')
-part2 = MIMEText(html, 'html')
+    # Record the MIME types of both parts - text/plain and text/html.
+    part1 = MIMEText(text, 'plain')
+    part2 = MIMEText(html, 'html')
 
-# Attach parts into message container.
-# According to RFC 2046, the last part of a multipart message, in this case
-# the HTML message, is best and preferred.
-message.attach(part1)
-message.attach(part2)
+    # Attach parts into message container.
+    # According to RFC 2046, the last part of a multipart message, in this case
+    # the HTML message, is best and preferred.
+    message.attach(part1)
+    message.attach(part2)
 
-# download, encode, and attach image
+    # download, encode, and attach image
 
-fp = open(str_path_img_history, 'rb')
-msgImage_history = MIMEImage(fp.read())
-fp.close()
+    if dict_data['history'] is not None:
+        
+        fp = open(str_path_img_history, 'rb')
+        msgImage_history = MIMEImage(fp.read())
+        fp.close()
 
-fp = open(str_path_img_age, 'rb')
-msgImage_age = MIMEImage(fp.read())
-fp.close()
+        # Define the image's ID as referenced above
+        msgImage_history.add_header('Content-ID', '<image1>')
+        message.attach(msgImage_history)
 
-# Define the image's ID as referenced above
+    if dict_data['age'] is not None:
+        
+        fp = open(str_path_img_age, 'rb')
+        msgImage_age = MIMEImage(fp.read())
+        fp.close()
 
-msgImage_history.add_header('Content-ID', '<image1>')
-message.attach(msgImage_history)
+        # Define the image's ID as referenced above
+        msgImage_age.add_header('Content-ID', '<image2>')
+        message.attach(msgImage_age)
 
-msgImage_age.add_header('Content-ID', '<image2>')
-message.attach(msgImage_age)
+    # connect to gmail
+    # for this to work, we need to turn ON less secure app access
 
-# connect to gmail
-# for this to work, we need to turn ON less secure app access
+    port = 465  # For SSL
 
-port = 465  # For SSL
-
-# Create a secure SSL context
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    server.login(sender_email, secrets.gbl_str_pw)
-    server.sendmail(
-        sender_email, receiver_email, message.as_string()
-    )
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, secrets.gbl_str_pw)
+        server.sendmail(
+            sender_email, receiver_email, message.as_string()
+        )
